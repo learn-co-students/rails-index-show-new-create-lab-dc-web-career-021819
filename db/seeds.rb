df76e88a4a18c3df214419a360a90de491ccc120
github username <github email address>
2019-04-28 15:05:58 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# Coupon.create(coupon_code: '', store: '')
+Coupon.destroy_all
+
+macys = Coupon.create(coupon_code: '30% off juniors', store: 'Macys, Metro Center')
+all_saints = Coupon.create(coupon_code: '20% off all jackets', store: 'All Saints, Georgetown')
+diesel = Coupon.create(coupon_code: '20% off denim', store: 'Diesel, Tysons Corner')
+target = Coupon.create(coupon_code: '70% off clearance', store: 'Target, Falls Church')
+helmut_lang = Coupon.create(coupon_code: '10% off sweaters', store: 'Helmut Lang, Meatpacking District')
+rag_bone = Coupon.create(coupon_code: '15% off leather leggings', store: 'Rag & Bone, Georgetown')
+alexander_wang = Coupon.create(coupon_code: '10% off shoes', store: 'Alexander Wang, NYC')
